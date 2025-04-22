df36ec85b74de29c14fcb84c6edb9479ed55b185
dakshcodez <daksharoracodez@gmail.com>
2025-04-23 05:04:47 +0530

    added view and create product endpoints

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "Hello there"
+    return {"message" : "Welcome"}
+
+products = []
+
+@app.post("/create_product")
+async def create_product(product: Product):
+    products.append(product)
+    return {"message" : "product has been added"}
+
+@app.get("/view_products")
+async def view_products():
+    return {"products": products}
