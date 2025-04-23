671b29b4d43f756b39c327db775fccbb395dff3f
dakshcodez <daksharoracodez@gmail.com>
2025-04-23 20:11:41 +0530

    added search product by category endpoint

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ async def root():
 
 products = []
 
+# Create a product
 @app.post("/create_product")
 async def create_product(product: Product):
     products.append(product)
     return {"message" : "product has been added"}
 
+# View all products
 @app.get("/view_products")
 async def view_products():
     return {"products": products}
+
+# Search for a product by category
+@app.get("/search_products/{category}")
+async def search_product_by_category(category: str):
+    search_res = []
+    for product in products:
+        if product.category == category:
+            search_res.append(product)
+    return {"products": search_res}
